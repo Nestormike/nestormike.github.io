f16f43b3eb7985d36d05c49e772c5a3a7a631279
mihail_nesterov <nesterov@utip.ru>
2019-08-16 11:25:07 +0500

    first commit

---
+++ precache-manifest.2fbad92bd82ec94dfd747fb13c8c58d1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "938c60fbcbcac30db62c6da4ef3c44e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b7e9fcf651884ba3b0c",
+    "url": "/static/css/main.335e812a.chunk.css"
+  },
+  {
+    "revision": "3ef35d8f97249e25f0bb",
+    "url": "/static/js/2.71ebdf12.chunk.js"
+  },
+  {
+    "revision": "6b7e9fcf651884ba3b0c",
+    "url": "/static/js/main.ffb61f5d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e0dd8c4c29aa26d98b27006a47b3edec",
+    "url": "/static/media/page1.e0dd8c4c.png"
+  },
+  {
+    "revision": "11d82ae0abdf62580a79675d9082ed4c",
+    "url": "/static/media/page2.11d82ae0.png"
+  },
+  {
+    "revision": "9f74fdf968b4a53b7ba51e1f45d2a010",
+    "url": "/static/media/page3.9f74fdf9.png"
+  },
+  {
+    "revision": "8e0bced7d002037d158237a3f5e3e016",
+    "url": "/static/media/page4.8e0bced7.png"
+  },
+  {
+    "revision": "258f351d4eb2968cfd147fec6cf79353",
+    "url": "/static/media/page5.258f351d.png"
+  },
+  {
+    "revision": "29f2ea8f4d534cec293d94cc0b0c4628",
+    "url": "/static/media/page6.29f2ea8f.png"
+  },
+  {
+    "revision": "8f170c311cb12bad992ae69c05007912",
+    "url": "/static/media/page7.8f170c31.png"
+  },
+  {
+    "revision": "11835ee3400566457220fdb549fa0e8f",
+    "url": "/static/media/page8.11835ee3.png"
+  }
+]);
